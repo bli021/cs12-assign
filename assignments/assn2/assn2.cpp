6275342bd82b69150acd19d13194da1427219d48
dande-011 <dande011@ucr.edu>
2014-01-22 14:31:23 -0800

    Started assignment 2

---
+++ assignments/assn2/assn2.cpp
@@ -0,0 +1,37 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file assignment1/part1.cpp 
+/// @brief Assignment 1 Part 1 for CS 12 Winter 2014
+///
+/// @author David Anderson [dande011@ucr.edu]
+/// @date <1/22/2014>
+/// @par Enrollment Notes 
+///     Lecture Section: 001
+/// @par
+///     Lab Section: 021
+/// @par
+///     TA: Nkenge Wheatland
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  =============== END ASSESSMENT HEADER ==================
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+	string inFile, outFile;
+	cout << "Enter the input filename: ";
+	cin >> inFile;
+	cout << endl;
+	cout << "Enter the output filename: ";
+	cin >> outFile;
+	
+	
+	
+	
+	return 0;
+}
