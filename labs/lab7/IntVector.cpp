96230497ed092dffc274d8d16b84e584f01e0836
dande011 <dande011@ucr.edu>
2014-02-19 12:54:09 -0800

    Subitted lab7 full credit

--- labs/lab7/IntVector.cpp
+++ labs/lab7/IntVector.cpp
@@ -1,18 +1,30 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file assignment1/main.cpp 
+/// @brief Assignment 2 for CS 12 Winter 2014
+///
+/// @author David Anderson [dande011@ucr.edu]
+/// @date <2/19/2014>
+/// @par Enrollment Notes 
+///     Lecture Section: 001
+/// @par
+///     Lab Section: 021
+/// @par
+///     TA: Nkenge Wheatland
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  =============== END ASSESSMENT HEADER ==================
 
 #include "IntVector.h"
 #include <cstdlib>
 
-IntVector::IntVector()
-:sz(0),cap(0)
-{
 
-}
 IntVector::IntVector(unsigned size):sz(size),cap(size)
 {
     data = new int[size];
     for(unsigned i=0; i < size; i++)
         data[i] = 0;
-
 }
 
 IntVector::IntVector(unsigned size, int value):sz(size),cap(size)
--- labs/lab7/IntVector.h
+++ labs/lab7/IntVector.h
@@ -1,3 +1,20 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file assignment1/main.cpp 
+/// @brief Assignment 2 for CS 12 Winter 2014
+///
+/// @author David Anderson [dande011@ucr.edu]
+/// @date <2/19/2014>
+/// @par Enrollment Notes 
+///     Lecture Section: 001
+/// @par
+///     Lab Section: 021
+/// @par
+///     TA: Nkenge Wheatland
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  =============== END ASSESSMENT HEADER ==================
 #ifndef _INTVECTOR_H_
 #define _INTVECTOR_H_
 
@@ -9,7 +26,7 @@ class IntVector
         int* data;
 
     public:
-        IntVector();//:sz(0),cap(0){delete data;}
+        IntVector():sz(0),cap(0),data(0){};
         IntVector(unsigned size);
         IntVector(unsigned size, int value);
         unsigned size() const{return sz;};
