8098c1105f3889f7df1c8894509a6d9be3b4c79b
David Anderson <dande011@ucr.edu>
2014-03-03 13:01:57 -0800

    Finished the first part of lab9

--- labs/lab9/main.cpp
+++ labs/lab9/main.cpp
@@ -16,20 +16,36 @@
 /// is ENTIRELY my own original work.
 //  =============== END ASSESSMENT HEADER ==================
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void reverse(string &s, int i)
+{
+    if(i >= s.size()/2)
+        return;
+    else
+    {
+	   swap( s.at(i), s.at( (s.size()-1) - i) );
+       reverse(s, i + 1);
+   }
+}
+
 void reverse(string &s)
 {
-	s.size()
+    reverse(s, 0);
 }
 
 int main()
 {
 	string temp;
-	cout << "Input a string: ";
+	cout << "Input a string:\t";
 	cin >> temp;
+
 	reverse(temp);
-	cout << "Reversed: " << temp << endl;
+	cout << "Reversed:\t" << temp << endl;
+
+    reverse(temp);
+    cout << "Re-Reversed:\t" << temp << endl;
 	return 0;
 }
